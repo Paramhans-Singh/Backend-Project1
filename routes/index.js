75ce95855b2bd1642dfdfdf8a4d3b9cbeeaa2f35
PARAMHANS <paramsingh11387@gmail.com>
2023-05-30 19:07:13 +0530

    Loaded controller and router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const homeController = require('../controllers/home_controller')
+
 console.log('route loaded');
+
+router.get('/', homeController.home);
 
 module.exports = router;
