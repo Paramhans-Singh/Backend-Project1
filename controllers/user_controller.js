77f80aee6b21c721a3dbfc6057467ce896388f87
PARAMHANS <paramsingh11387@gmail.com>
2023-05-31 18:09:05 +0530

    Made SignIn/SignUp Portal

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,3 +5,28 @@ module.exports.profile = function(req,res){
         title: "User/Profile"
     });
 }
+
+
+// render the sign up page 
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign UP"
+    });
+}
+
+// render the sign in page 
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    });
+}
+
+// get the sign up data 
+module.exports.create = function(req,res){
+    //TODO
+}
+
+// sign in and create a session for a user
+module.exports.createSession= function(req,res){
+    //TODO
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 router.get('/profile',userController.profile)
 
+router.get('/sign-up',userController.signUp)
+router.get('/sign-in',userController.signIn)
+
 console.log('Users')
 
 module.exports = router;
